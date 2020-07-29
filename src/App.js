227124ab6649c6e7b2d3625933a31c29b13f6922
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-07-28 21:19:33 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p>This app will be for testing out multiple forms</p>
+
+      <p>&copy; Kintsugi L<span className='kintsugi'>a</span>bs {new Date().getFullYear()}</p>
+    </div>
+  );
+}
+
+export default App;
